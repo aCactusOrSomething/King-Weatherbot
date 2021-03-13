891686194a53dcc6edb6b588d716bf11e630efa4
cactusEight <whudsonmiller@gmail.com>
2021-03-13 22:18:56 +0000

    captains :)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.once('ready', () => {
   console.log('Ready!');
-  client.user.setActivity('https://King-WeatherBot.cactuseight.repl.co'); //this sets what weatherbot is currently "Playing."
+  client.user.setActivity('https://King-Weatherbot.cactuseight.repl.co'); //this sets what weatherbot is currently "Playing."
 });
 
 client.on('message', message => { //weatherbot checks EVERY message sent that it could see. Usually you'd look for a command, but not with this bot.
@@ -40,7 +40,7 @@ client.login(process.env.TOKEN);
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end(`My invite link is: https://discord.com/oauth2/authorize?client_id=769783799306780732&scope=bot \nFor every message sent in your server, I may decide to Incinerate the sender. \nIf you create a role labelled "Incinerated" (with the first letter capitalized), and give me permission to manage roles, I will apply it to any user I incinerate. (This also makes incineration permanent - I cannot incinerate someone with the Incinerated role). \nIf you have questions please direct them to discord user @heroguy15#9469 !\nHave fun!~\n\nCREDIT:\nDelapouite of Game-Icons.net: Icon, Licensed under https://creativecommons.org/licenses/by/3.0/\nThe Game Band: Inspiration, and Blaseball.com`);
+  res.end(`My invite link is: https://discord.com/oauth2/authorize?client_id=769783799306780732&scope=bot \nFor every message sent in your server, I may decide to Incinerate the sender. \nIf you create a role labelled "Incinerated" (with the first letter capitalized), and give me permission to manage roles, I will apply it to any user I incinerate. (This also makes incineration permanent - I cannot incinerate someone with the Incinerated role). \nIf you have questions please direct them to discord user @heroguy15#9469 !\nHave fun!~\n\nCREDIT:\nDelapouite of Game-Icons.net: Icon, Licensed under https://creativecommons.org/licenses/by/3.0/\nThe Game Band: Inspiration, and Blaseball.com\nBoston Flowers Captains: Development`);
 });
 server.listen(3000);
 
