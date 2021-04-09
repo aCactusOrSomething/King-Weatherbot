f736fcb905ffd20e8c980dd150e6611250b80b16
aCactusOrSomething <31144053+aCactusOrSomething@users.noreply.github.com>
2021-04-09 03:18:26 -0500

    help fix

--- code/commands/info/help.js
+++ code/commands/info/help.js
@@ -14,7 +14,7 @@ module.exports = {
         if(!args.length) {
             data.push("Here's a list of all my commands:");
             data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nUse \`${prefix}${name} ${usage}\` to get info on a specific command!`);
+            data.push(`\nUse \`${prefix}${this.name} ${this.usage}\` to get info on a specific command!`);
 
             message.reply(data, {split: true});
         }
