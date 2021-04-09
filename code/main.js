82b71f0a31569f2c6ddb68eb5223e9e44c09925f
aCactusOrSomething <31144053+aCactusOrSomething@users.noreply.github.com>
2021-04-09 01:18:06 -0500

    oop

--- code/main.js
+++ code/main.js
@@ -6,7 +6,7 @@ const token = process.env.TOKEN;
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./code/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
